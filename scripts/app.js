bd71ecf45bd41bac3038353c4552717f8ef32d54
Diaresta <TheAndrewRobles@yahoo.com>
2020-12-11 20:45:38 -0500

    added accessibility options

--- scripts/app.js
+++ scripts/app.js
@@ -16,15 +16,14 @@ function showSlides(n) {
     if (mediaQ.matches) {
         var i;
         var slides = document.getElementsByClassName("setPhoto");
-        var dots = document.getElementsByClassName("dot");
+
         if (n > slides.length) {slideIndex = 1}
         if (n < 1) {slideIndex = slides.length}
+
         for (i = 0; i < slides.length; i++) {
             slides[i].style.display = "none";
         }
-        for (i = 0; i < dots.length; i++) {
-            dots[i].className = dots[i].className.replace(" active", "");
-        }
+
         slides[slideIndex-1].style.display = "block";
         dots[slideIndex-1].className += " active";
     }
