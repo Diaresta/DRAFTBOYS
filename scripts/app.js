2780c9e8620bc45cdc56d707e6e16d3f49eb2d93
Diaresta <TheAndrewRobles@yahoo.com>
2020-12-16 00:29:08 -0500

    fixed to/from set slideshow when resizing window

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,5 @@
 var slideIndex = 1;
 showSlides(slideIndex);
-// var mediaQ = window.matchMedia("(max-width: 630px)");
 
 // Next/previous controls
 function plusSlides(n) {
@@ -12,6 +11,7 @@ function currentSlide(n) {
     showSlides(slideIndex = n);
 }
 
+// Slideshow when <= 723px
 function showSlides(n) {
     if (window.matchMedia("(max-width: 723px)").matches) {
         var slides = document.getElementsByClassName("setList");
@@ -24,8 +24,24 @@ function showSlides(n) {
         }
 
         slides[slideIndex-1].style.display = "block";
-    } else {
     }
 } 
 
-window.addEventListener('resize', showSlides);
+// Inline set photos when >=724px
+function showSets(n) {
+    if (window.matchMedia("(min-width: 724px)").matches) {
+        var setPhotos = document.getElementsByClassName("setList");
+
+        if (n > setPhotos.length) {slideIndex = 1}
+        if (n < 1) {slideIndex = setPhotos.length}
+
+        for (i = 0; i < setPhotos.length; i++) {
+            setPhotos[i].style.display = "inline-block";
+        }
+    }
+} 
+
+window.addEventListener('resize', () => {
+    showSlides();
+    showSets();
+});
