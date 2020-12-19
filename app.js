bcf41d230e5e5ec464f4196fc6da5ef1e2a7e587
Diaresta <TheAndrewRobles@yahoo.com>
2020-12-19 02:27:00 -0500

    setting up express

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log('Hello World');
+const express = require('express');
+const http = require('http');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.listen(3000, () => {
+    console.log('Port Started');
+});
+
+console.log('node working');
+
+
+
+// app.use(express.static(__dirname + '/public'));
+
+// http.createServer(app).listen(3000);
+
+// maybe use restify?
