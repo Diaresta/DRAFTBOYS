b3c3e46d2d92a80e59a768cd605eba101fad796a
Diaresta <TheAndrewRobles@yahoo.com>
2020-12-21 22:24:38 -0500

    testing mtg card api

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-
+const mtg = require('mtgsdk')
 // const fs = require('fs');   
 // const http = require('http');
 
@@ -21,6 +21,21 @@ app.get('/draft', (req, res) => {
 
 app.listen(3000, () => {
     console.log('Listening on port 3000');
+});
+
+// find specific card
+mtg.card.find(3)
+.then(result => {
+    console.log(result.card.name) // "Black Lotus"
+    console.log(result.card.imageUrl)
+});
+
+// testing
+mtg.set.where({ block: 'Shadows over Innistrad', border: 'black' })
+.then(sets => {
+  console.log(sets[0].name) // "Welcome Deck 2016"
+  console.log(sets[1].name) // "Shadows over Innistrad"
+  console.log(sets[2].block) // "Eldritch Moon"
 });
 
 module.exports = app;
