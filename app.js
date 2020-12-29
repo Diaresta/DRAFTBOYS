d29e51ecbeed9f0e9928712bbe2a5c9cf93617a6
Diaresta <TheAndrewRobles@yahoo.com>
2020-12-28 20:14:16 -0500

    planning out set parsing

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 // Imports
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const path = require('path');
+const fs = require('fs');
 const mtg = require('mtgsdk')
-// const fs = require('fs');   
-// const http = require('http');
 
 // Static Files
 app.use(express.static(path.join(__dirname, 'static')));
+app.use(bodyParser.json());
 
 // Routing
 app.get('/', (req, res) => {
@@ -23,14 +24,15 @@ app.listen(3000, () => {
     console.log('Listening on port 3000');
 });
 
-// find specific card
+// ------------------------------------  finding cards json -------------------------------------------------
+// Find specific card
 mtg.card.find(3)
 .then(result => {
     console.log(result.card.name) // "Black Lotus"
     console.log(result.card.imageUrl)
 });
 
-// testing
+// Testing
 mtg.set.where({ block: 'Shadows over Innistrad', border: 'black' })
 .then(sets => {
   console.log(sets[0].name) // "Welcome Deck 2016"
@@ -42,10 +44,44 @@ mtg.set.where({ block: 'Shadows over Innistrad', border: 'black' })
 // https://api.scryfall.com/sets/znr
 // https://api.scryfall.com/cards/search?order=set&q=e%3Aznr&unique=prints
 
+// ------------------------------------ card rarity -------------------------------------------------
+// const common;
+// const uncommon;
+// const rare;
+// const mythic;
+// how draftpod does their card function
+// export rare(card) => {
+//     return rarity(["rare"](card));
+// }
+
+
+
+// how draftpod does their rare/mythic choice
+// packRareChoice(card) {
+//     if (Math.random() <= (1/8)) {
+//         return mythic(card);
+//     } else {
+//         return rare(card);
+//     }
+// }
+
+// ------------------------------------ set parsing -------------------------------------------------
+const eldData = fs.readFileSync('static/sets/eld.json');
+const eldSet = JSON.parse(eldData);
+
+const ikoData = fs.readFileSync('static/sets/iko.json');
+const ikoSet = JSON.parse(ikoData);
+
+const m21Data = fs.readFileSync('static/sets/m21.json');
+const m21Set = JSON.parse(m21Data);
+
+const thbData = fs.readFileSync('static/sets/thb.json');
+const thbSet = JSON.parse(thbData);
+
+const znrData = fs.readFileSync('static/sets/znr.json');
+const znrSet = JSON.parse(znrData);
+
+
+console.log('node w orking');
+
 module.exports = app;
-
-console.log('node working');
-
-
-
-// app.use(express.static(__dirname + '/public'));
